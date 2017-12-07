3d9bc280768c71239aa14b410b32618e20876fd4
Unknown <clementserrano3@gmail.com>
2017-12-07 22:27:37 +0100

    Correction nav + création route map

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
+import { MapComponent } from './map/map.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/accueil', pathMatch: 'full' },
-  { path: 'accueil', component: AccueilComponent }
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'map', component: MapComponent }
 ];
 
 @NgModule({
