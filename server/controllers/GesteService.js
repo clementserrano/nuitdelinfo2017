d6ff65ebecddec9519b77f47989af434c0bebf05
Maxime <maxime.belmon@gmail.com>
2017-12-08 01:38:04 +0100

    Go Back-end
    Ajout des premiers éléments bd + corrections bugs mineux

--- server/controllers/GesteService.js
+++ server/controllers/GesteService.js
@@ -1,5 +1,24 @@
 'use strict';
 
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+// Connect
+const connection = (closure) => {
+    return MongoClient.connect('mongodb://127.0.0.1:27017/nuitdelinfo2017', (err, db) => {
+        if (err) return console.log(err);
+
+        closure(db);
+    });
+};
+
+// Response handling
+let response = {
+    status: 200,
+    data: [],
+    message: null
+};
+
 exports.addGeste = function(args, res, next) {
   /**
    * Ajouter un geste
@@ -30,21 +49,18 @@ exports.deleteGeste = function(args, res, next) {
 }
 
 exports.getGestes = function(args, res, next) {
-  /**
-   * Retourne la liste des gestes qui sauvent
-   * Retourne la liste des gestes qui sauvent
-   *
-   * returns Map
-   **/
-  var examples = {};
-  examples['application/json'] = {
-  "key" : 0
+  connection((db) => {
+        db.collection('gestes')
+            .find()
+            .toArray()
+            .then((gestes) => {
+                response.data = gestes;
+                res.json(response);
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+	});
 };
-  if (Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  } else {
-    res.end();
-  }
-}
+
 
